ccd38e6ed52ecd28d084da654358d37789e83746
Alexey Murz Korepov <MurzNN@gmail.com>
2021-02-11 09:04:59 +0300

    dump-output

--- db-dump.js
+++ db-dump.js
@@ -3,10 +3,10 @@
 const fs = require("fs");
 const prettyBytes = require("pretty-bytes");
 
-const { config, dbDumpCreate } = require(__dirname + "/lib/utils");
+const { config, dbDump } = require(__dirname + "/lib/utils");
 
 console.log("Creating dump of database to file " + config.dumpFile + "...");
-dbDumpCreate(config.siteCurrent.dbConnectionString, config.dumpFile);
+dbDump(config.siteCurrent.dbConnectionString, config.dumpFile);
 dumpFileStat = fs.statSync(config.dumpFile);
 console.log(
   "Database dump file created: " +
--- db-restore.js
+++ db-restore.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const prettyBytes = require("pretty-bytes");
 
-const { config, dbDumpRestore } = require(__dirname + "/lib/utils");
+const { config, dbRestore } = require(__dirname + "/lib/utils");
 
 if (!fs.existsSync(config.dumpFile)) {
   console.log("ERROR: Database dump file " + config.dumpFile + " not exists!");
@@ -13,5 +13,5 @@ if (!fs.existsSync(config.dumpFile)) {
 dumpFileStat = fs.statSync(config.dumpFile);
 console.log("Restoring database from file " + config.dumpFile
   + "(size: " + prettyBytes(dumpFileStat.size) + " to current db...");
-dbDumpRestore(config.siteCurrent.dbConnectionString, config.dumpFile);
+dbRestore(config.siteCurrent.dbConnectionString, config.dumpFile);
 console.log("Database successfully restored.");
