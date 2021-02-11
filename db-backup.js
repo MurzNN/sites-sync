997e3269719235b479ac0f56ff24050a18882c14
Alexey Murz Korepov <MurzNN@gmail.com>
2021-02-11 09:11:26 +0300

    fixes

---
+++ db-backup.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const prettyBytes = require("pretty-bytes");
+
+const { config, dbDump } = require(__dirname + "/lib/utils");
+
+console.log("Creating dump of database to file " + config.dumpFile + "...");
+dbBackup(config.siteCurrent.dbConnectionString, config.dumpFile);
+dumpFileStat = fs.statSync(config.dumpFile);
+console.log(
+  "Database dump file created: " +
+    config.dumpFile +
+    "; size: " +
+    prettyBytes(dumpFileStat.size)
+);
--- db-dump.js
+++ db-dump.js
@@ -5,12 +5,4 @@ const prettyBytes = require("pretty-bytes");
 
 const { config, dbDump } = require(__dirname + "/lib/utils");
 
-console.log("Creating dump of database to file " + config.dumpFile + "...");
 dbDump(config.siteCurrent.dbConnectionString, config.dumpFile);
-dumpFileStat = fs.statSync(config.dumpFile);
-console.log(
-  "Database dump file created: " +
-    config.dumpFile +
-    "; size: " +
-    prettyBytes(dumpFileStat.size)
-);
