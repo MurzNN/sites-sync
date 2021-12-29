0c50b92309f7bb0142a9939d856c221675915f57
Alexey Murz Korepov <MurzNN@gmail.com>
2021-12-29 14:37:46 +0300

    Workaround for empty db access parts

--- src/lib/dbAdapters/postgresql.ts
+++ src/lib/dbAdapters/postgresql.ts
@@ -16,7 +16,9 @@ export default class PostgresqlDbAdapter
       this.dbPassFile = getTmpFilename();
       writeFileSync(
         this.dbPassFile,
-        `${this.connection.host}:${this.connection.port}:${this.connection.dbName}:${this.connection.username}:${this.connection.password}`,
+        `${this.connection.host ?? "localhost"}:${this.connection.port ?? ""}:${
+          this.connection.dbName
+        }:${this.connection.username ?? ""}:${this.connection.password ?? ""}`,
         { mode: "600" }
       );
     }
